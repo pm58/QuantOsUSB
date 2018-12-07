de3ae1fb2ec2e1a87baf446e80d3cf756afe5ccf
pm58 <apple.zy@me.com>
2018-12-07 21:21:17 +0800

    修复mongodb文件开启限制256个奔溃问题

--- QuantOsUSB.sparseimage/QuantOsUSB/Kivy3.app/Contents/MacOS/mongod.sh
+++ QuantOsUSB.sparseimage/QuantOsUSB/Kivy3.app/Contents/MacOS/mongod.sh
@@ -1,4 +1,8 @@
 
+echo "开启最大化文件256限制到4096"
+ulimit -a
+ulimit -n 4096
+ulimit -a
 echo "启动 mongodb "
 echo "使用组合键 control+C 可以退出！！！"
 /Volumes/data/data/mongodb/bin/./mongod --ipv6 -f /Volumes/data/data/mongodb/bin/mongodb.conf
